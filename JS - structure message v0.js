1e5e938d2d7e4bdd56a8d913665ce4fc3cdc873d
Jean-Baptiste Herold <herold.jb@gmail.com>
2021-05-19 10:47:52 +0200

    Create JS - structure message v0.js

---
+++ JS - structure message v0.js
@@ -0,0 +1,51 @@
+
+const type = ["humor", "philosophy", "poetry", "economy", "society", "nature", "proverb", "unclassifiable"];
+/*plus de catégorie possible sur : https://www.lesmotspositifs.com/citations-par-categories/*/
+/*utilisation d'un webscraping pour récupérer les citations par catégorie sur ce site en python possible*/
+
+const author = ["author1", "author2", "author3", "..."];
+/*si utilisation d'un web scrap, cleaning data et création de listes nettoyées via python*/
+
+/*içi aussi création d'un objet json possible via python si web scrap puis récupération de la data en js içi*/
+const citation = {
+    "humor" : {
+        "author1" : ["I am a citation", "I am a citation", "..."],
+        "author2" : ["I am a citation", "I am a citation", "..."],
+        "author3" : ["I am a citation", "I am a citation", "..."]
+    },
+    "philosophy": {
+        "author1" : ["I am a citation", "I am a citation", "..."],
+        "author2" : ["I am a citation", "I am a citation", "..."],
+        "author3" : ["I am a citation", "I am a citation", "..."]
+    },
+    "poetry": {
+        "author1" : ["I am a citation", "I am a citation", "..."],
+        "author2" : ["I am a citation", "I am a citation", "..."],
+        "author3" : ["I am a citation", "I am a citation", "..."]
+    },
+    "economy": {
+        "author1" : ["I am a citation", "I am a citation", "..."],
+        "author2" : ["I am a citation", "I am a citation", "..."],
+        "author3" : ["I am a citation", "I am a citation", "..."]
+    },
+    "society": {
+        "author1" : ["I am a citation", "I am a citation", "..."],
+        "author2" : ["I am a citation", "I am a citation", "..."],
+        "author3" : ["I am a citation", "I am a citation", "..."]
+    },
+	"nature": {
+        "author1" : ["I am a citation", "I am a citation", "..."],
+        "author2" : ["I am a citation", "I am a citation", "..."],
+        "author3" : ["I am a citation", "I am a citation", "..."]
+    },
+	"proverb": {
+        "author1" : ["I am a citation", "I am a citation", "..."],
+        "author2" : ["I am a citation", "I am a citation", "..."],
+        "author3" : ["I am a citation", "I am a citation", "..."]
+    },
+    "unclassifiable": {
+        "author1" : ["I am a citation", "I am a citation", "..."],
+        "author2" : ["I am a citation", "I am a citation", "..."],
+        "author3" : ["I am a citation", "I am a citation", "..."]
+    },
+};
